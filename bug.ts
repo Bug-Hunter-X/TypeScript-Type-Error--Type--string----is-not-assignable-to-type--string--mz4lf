6893bb94fd567254a73d2823c34a01010192c10f
BugHunter-X <askzenai@gmail.com>
2025-01-07 15:04:04 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = ["Jane", "Doe"];
+
+console.log(greeter(user)); // This will cause an error because the function expects a string, not an array of strings.
